614f4bd100801ffd171316a484a1668cd05e24e9
MadisAr <madis.arro1@gmail.com>
2024-12-01 18:31:57 +0200

    uued pildid ja tekst esilehel jne

---
+++ javascript/index.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded" , function() {
+
+    const pildid = document.querySelector("#contain");
+    let index = 0;
+    const tekstid = ["Kuula Heart part strings", "Kuula Pearl name"];
+    const albumi_nimi = document.getElementById("albumi_nimi")
+    
+    function uuenda(){
+        // liigutab elemente vasakule, elemendi alast valja ja peidab need elemendid.
+        if (index==1) {
+            index = 0;
+        } else {
+            index = 1;
+        }
+        pildid.style.transform = `translateX(-${index * 100}%)`;
+        albumi_nimi.innerText = tekstid[index]
+    }
+    
+    setInterval(uuenda, 3500);
+});
